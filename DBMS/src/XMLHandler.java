0f0313376d2b269a717346315e8247fff60a2704
Ayman Geneidy <Ayman.Geneidy@gmail.com>
2013-10-31 23:31:52 +0200

    ana 3amlt el load

--- DBMS/src/XMLHandler.java
+++ DBMS/src/XMLHandler.java
@@ -36,39 +36,48 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class XMLHandler {
+
+	private ColumnIdentifier[] colID;
+	private XMLEventReader eventReader;
+	private XMLEventWriter eventWriter;
+
 	private String tableName;
-	private ArrayList<Class<?>> columnsType;
 	private ArrayList<String> columnsName;
 	private ArrayList<Record> records;
-	private XMLEventWriter eventWriter;
+
 	private XMLEventFactory eventFactory;
 
-	public XMLHandler(String dbName, Table table) {
-		records = new ArrayList<Record>();
-		setAttributes(table);
+	public XMLHandler(File file, ColumnIdentifier[] colID, boolean write) {
+		this.colID = colID;
 		try {
-			createXML();
+			if (!write) {
+				readXML(file);
+			} else {
+				writeXML(file);
+			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("some error!");
 		}
 
 	}
 
-	private void setAttributes(Table table) {
-		// TODO Auto-generated method stub
-		tableName = table.getTableName();
-		columnsName = (ArrayList<String>) table.getColumnsNames();
-		columnsType = (ArrayList<Class<?>>) table.getColumnsTypes();
+	public void readXML(File file) throws Exception {
+		// First, create a new XMLInputFactory
+		XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+		// Setup a new eventReader
+		InputStream in = new FileInputStream(file);
+		eventReader = inputFactory.createXMLEventReader(in);
+		eventReader.nextEvent();
+		eventReader.nextEvent();
+
 	}
 
-	private void createXML() throws Exception {
+	private void writeXML(File file) throws Exception {
 		// create an XMLOutputFactory
 		XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
 		// create XMLEventWriter
 		eventWriter = outputFactory.createXMLEventWriter(new FileOutputStream(
-				tableName + ".xml"));
-		// System.out.println(new FileOutputStream(tableName + ".xml"));
+				file));
 		// create an EventFactory
 		eventFactory = XMLEventFactory.newInstance();
 		StartDocument startDocument = eventFactory.createStartDocument();
@@ -78,12 +87,6 @@ public class XMLHandler {
 				"", "config");
 		eventWriter.add(configStartElement);
 		eventWriter.add(eventFactory.createDTD("\n"));
-		// Write the different nodes
-		addAllRecordes();
-		// create and write end tag
-
-		eventWriter.add(eventFactory.createEndDocument());
-		eventWriter.close();
 	}
 
 	private void addAllRecordes() throws XMLStreamException {
@@ -116,16 +119,16 @@ public class XMLHandler {
 		eventWriter.add(end);
 	}
 
-	public void addRecord(Record record) throws XMLStreamException {
-		records.add(record);
-		try {
-			createXML();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// readConfig(tableName+".xml");
-	}
+	// public void addRecord(Record record) throws XMLStreamException {
+	// records.add(record);
+	// try {
+	// createXML();
+	// } catch (Exception e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	// // readConfig(tableName+".xml");
+	// }
 
 	private void createNode(String name, String value)
 			throws XMLStreamException {
@@ -148,8 +151,57 @@ public class XMLHandler {
 	}
 
 	// event driven
-	public Record readNextRecord() {
+	public Record readNextRecord() throws Exception {
+		String[] columnsNames = new String[colID.length];
+		Object[] values = new Object[colID.length];
+		// read <Record>
+		eventReader.nextEvent();
+		eventReader.nextEvent();
+		eventReader.nextEvent();
+		
+		for (int i = 0; i < colID.length; i ++){
+			XMLEvent event = eventReader.nextEvent();
+			if (event.isStartElement()) {
+				System.out.println(event.asStartElement().getName());
+				//System.out.println(event.asCharacters().getData());
+			} else {
+				System.out.println("not start");
+				i--;
+			}
+		}
+		/*
+		// read elements
+		while (eventReader.hasNext()) {
+			XMLEvent event = eventReader.nextEvent();
+			if (event.isStartElement()) {
+				StartElement startElement = event.asStartElement();
+				System.out.println(event.asStartElement().getName());
+//				System.out.println(event.asCharacters().getData());
+				// If we have an item element, we create a new item
 
+				if (event.asStartElement().getName().getLocalPart()
+						.equals(colID[i].getColumnName())) {
+					event = eventReader.nextEvent();
+					// item.add(event.asCharacters().getData());
+					columnsNames[i] = colID[i].getColumnName();
+					values[i++] = event.asCharacters().getData();
+					continue;
+				}
+
+				// If we reach the end of an item element, we add it to the list
+				if (event.isEndElement()) {
+					EndElement endElement = event.asEndElement();
+					if (endElement.getName().getLocalPart().equals("Record")) {
+						System.out.println("ended");
+						// System.out.println(values[0]);
+						return (new Record(columnsNames, values));
+					}
+				}
+
+			}
+
+		}*/
+		System.out.println(";S");
 		return null;
 	}
 
@@ -157,62 +209,20 @@ public class XMLHandler {
 
 	}
 
-	public void readConfig(String configFile) {
+	public void readConfig(String configFile) throws Exception {
 
-		try {
-			// First, create a new XMLInputFactory
-			XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-			// Setup a new eventReader
-			InputStream in = new FileInputStream(configFile);
-			XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
-			// read the XML document
-			ArrayList<String> item = new ArrayList<String>();
-			ArrayList<String> names = new ArrayList<String>();
+		// First, create a new XMLInputFactory
+		XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+		// Setup a new eventReader
+		InputStream in = new FileInputStream(configFile);
+		XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+		// read the XML document
 
-			while (eventReader.hasNext()) {
-				XMLEvent event = eventReader.nextEvent();
+	}
 
-				if (event.isStartElement()) {
-
-					StartElement startElement = event.asStartElement();
-					// If we have an item element, we create a new item
-
-					if (startElement.getName().getLocalPart().equals("Record")) {
-						item = new ArrayList<String>();
-						names = new ArrayList<String>();
-						// attribute to our object
-					}
-
-					event = eventReader.nextEvent();
-					// if(event.asStartElement().getName().getLocalPart()!=null)System.out.println("a77a");;
-					// System.out.println(in.toString());
-					for(String j : columnsName)
-						if (event.asStartElement().getName().getLocalPart()
-								.equals(j)) {
-	
-							item.add(event.asCharacters().getData().toString());
-							System.out.println("Item" + item);
-							System.out.println(event.asCharacters().getData()
-									.toString());
-							names.add(event.asStartElement().getName()
-									.getLocalPart());
-					}
-				}
-
-				// If we reach the end of an item element, we add it to the list
-				if (event.isEndElement()) {
-					EndElement endElement = event.asEndElement();
-					if (endElement.getName().getLocalPart() == "Record") {
-						// items.add(item);
-					}
-				}
-
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (XMLStreamException e) {
-			e.printStackTrace();
-		}
+	public void closeXML() throws XMLStreamException {
+		eventWriter.add(eventFactory.createEndDocument());
+		eventWriter.close();
 	}
 
 }
