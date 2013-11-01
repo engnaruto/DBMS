758b0ce0415333388fdd2e7cded2fdffef730ab2
iocoder <iocoder@aol.com>
2013-11-01 23:11:37 +0200

    supress warnings

--- DBMS/src/DTDGenerator.java
+++ DBMS/src/DTDGenerator.java
@@ -4,8 +4,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.xml.parsers.SAXParserFactory;
+
+@SuppressWarnings("all")
 
 /**
  * DTDGenerator<BR>
