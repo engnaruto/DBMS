d01d3f4e41a2a664b49f68ef48eb61646c14080a
iocoder <iocoder@aol.com>
2013-11-01 20:47:25 +0200

    pushEverything

---
+++ DBMS/src/XMLHandlerTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class XMLHandlerTest {
+
+	@Test
+	public void testReadNextRecord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteNextRecord() {
+		fail("Not yet implemented");
+	}
+
+}
